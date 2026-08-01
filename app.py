477e2cf9b5c50aba2964577dc2a411c1ebdc88af
Rohini Kshirasagar <rohinikshirasagar123@gmail.com>
2026-08-01 17:14:37 +0530

    Update model filename to LR_model.pkl

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 
 # Load model and scaler
-model = joblib.load("model.pkl")
+model = joblib.load("LR_model.pkl")
 scaler = joblib.load("scaler.pkl")
 
 st.title("Titanic Survival Prediction")
